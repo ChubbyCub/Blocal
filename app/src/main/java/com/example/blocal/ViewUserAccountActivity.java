e0e7a24e665751f4942a7913f675ed95132579d7
Elise Pham <elise.pham88@gmail.com>
2019-07-22 19:52:19 -0700

    ok on layout for user profile page

--- app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
+++ app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
@@ -3,12 +3,52 @@ package com.example.blocal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ViewUserAccountActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ViewUserAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button manageTransactionButton;
+    private Button editProfile;
+    private Button signOutButton;
+
+    FirebaseFirestore db;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_view_user_account );
+        manageTransactionButton = findViewById ( R.id.manage_transaction_btn );
+        editProfile = findViewById ( R.id.edit_profile_btn );
+        signOutButton = findViewById ( R.id.sign_out_btn );
+
+        manageTransactionButton.setOnClickListener ( this );
+        editProfile.setOnClickListener ( this );
+        signOutButton.setOnClickListener ( this );
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.manage_transaction_btn:
+                break;
+            case R.id.edit_profile_btn:
+                break;
+            case R.id.sign_out_btn:
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+
     }
 }
