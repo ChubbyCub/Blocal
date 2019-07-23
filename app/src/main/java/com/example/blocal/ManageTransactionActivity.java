5656bbc4f942951e0b1ecffcfeb0db779d2c2dda
Elise Pham <elise.pham88@gmail.com>
2019-07-23 11:02:16 -0700

    added tab layout

--- app/src/main/java/com/example/blocal/ManageTransactionActivity.java
+++ app/src/main/java/com/example/blocal/ManageTransactionActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 
-public class ManageTransactionActivity extends AppCompatActivity {
+public class ManageTransactionActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,30 @@ public class ManageTransactionActivity extends AppCompatActivity {
         final Bundle stringArrayList = getIntent().getExtras ();
         ArrayList<String> productIds = stringArrayList.getStringArrayList( "productIds" );
         Log.d("anything in here? ", productIds.toString ());
+
+        tabHost = (TabHost) findViewById ( R.id.tabHost );
+        tabHost.setup();
+
+        TabHost.TabSpec spec = tabHost.newTabSpec ( "Sell" );
+        spec.setContent ( R.id.tab1 );
+        spec.setIndicator("Sell");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec ( "Buy" );
+        spec.setContent ( R.id.tab2 );
+        spec.setIndicator("Buy");
+        tabHost.addTab(spec);
+
+        tabHost.setOnTabChangedListener ( this );
+    }
+
+    @Override
+    public void onTabChanged(String s) {
+        switch(s) {
+            case "Sell":
+                break;
+            case "Buy":
+                break;
+        }
     }
 }
