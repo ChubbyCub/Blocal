68468802882d2193246f4bbf69ae6236551dd15b
Elise Pham <elise.pham88@gmail.com>
2019-07-22 20:04:40 -0700

    fixed more details on the user profile page

--- app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
+++ app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
@@ -5,14 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ViewUserAccountActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button manageTransactionButton;
-    private Button editProfile;
+    private TextView manageTransaction;
+    private TextView editProfile;
     private Button signOutButton;
 
     FirebaseFirestore db;
@@ -24,11 +25,11 @@ public class ViewUserAccountActivity extends AppCompatActivity implements View.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_view_user_account );
-        manageTransactionButton = findViewById ( R.id.manage_transaction_btn );
+        manageTransaction = findViewById ( R.id.manage_transaction_btn );
         editProfile = findViewById ( R.id.edit_profile_btn );
         signOutButton = findViewById ( R.id.sign_out_btn );
 
-        manageTransactionButton.setOnClickListener ( this );
+        editProfile.setOnClickListener ( this );
         editProfile.setOnClickListener ( this );
         signOutButton.setOnClickListener ( this );
 
