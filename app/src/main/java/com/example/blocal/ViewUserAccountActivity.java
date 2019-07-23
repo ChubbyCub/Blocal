ea31b17285c0b9d15d1f08e5e0e023b456b56f5a
Elise Pham <elise.pham88@gmail.com>
2019-07-22 20:20:58 -0700

    added profile placeholder

--- app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
+++ app/src/main/java/com/example/blocal/ViewUserAccountActivity.java
@@ -2,6 +2,8 @@ package com.example.blocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +22,14 @@ public class ViewUserAccountActivity extends AppCompatActivity implements View.O
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
     String currentUserId;
+    Uri userProfileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_view_user_account );
+        setRequestedOrientation ( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT );
+
         manageTransaction = findViewById ( R.id.manage_transaction_btn );
         editProfile = findViewById ( R.id.edit_profile_btn );
         signOutButton = findViewById ( R.id.sign_out_btn );
@@ -50,6 +55,9 @@ public class ViewUserAccountActivity extends AppCompatActivity implements View.O
     @Override
     protected void onStart() {
         super.onStart ();
-
+        firebaseAuth = FirebaseAuth.getInstance ();
+        user = firebaseAuth.getCurrentUser ();
+        currentUserId = user.getUid ();
+        userProfileUri = user.getPhotoUrl ();
     }
 }
