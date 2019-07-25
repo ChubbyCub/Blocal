ce9e9fbeef677f91263cb1988cfb58de8eba8b6d
Elise Pham <elise.pham88@gmail.com>
2019-07-25 13:39:17 -0700

    trying to merge

--- app/src/main/java/com/example/blocal/PostProductActivity.java
+++ app/src/main/java/com/example/blocal/PostProductActivity.java
@@ -76,7 +76,7 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
     private FirebaseFirestore db;
     private FirebaseAuth fireBaseAuth;
     private FirebaseUser user;
-    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private StorageReference storageReference = FirebaseStorage.getInstance ().getReference ();
 
     private List<String> categories;
     private String chosenCategory;
@@ -86,141 +86,140 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_product);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_post_product );
+        setRequestedOrientation ( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT );
 
-        takePhotoButton = findViewById(R.id.take_photo_button);
-        uploadPhotoButton = findViewById(R.id.upload_photo_button);
-        productNameText = findViewById(R.id.product_name_post);
-        productCategorySpinner = findViewById(R.id.category_spinner_post);
-        productDescriptionText = findViewById(R.id.product_description_post);
-        autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById (R.id.autocomplete_fragment);
-        productPriceText = findViewById(R.id.product_price_post);
-        postProductButton = findViewById(R.id.post_product_button);
-        productImageView = findViewById(R.id.product_image_post);
+        takePhotoButton = findViewById ( R.id.take_photo_button );
+        uploadPhotoButton = findViewById ( R.id.upload_photo_button );
+        productNameText = findViewById ( R.id.product_name_post );
+        productCategorySpinner = findViewById ( R.id.category_spinner_post );
+        productDescriptionText = findViewById ( R.id.product_description_post );
+        autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager ().findFragmentById ( R.id.autocomplete_fragment );
+        productPriceText = findViewById ( R.id.product_price_post );
+        postProductButton = findViewById ( R.id.post_product_button );
+        productImageView = findViewById ( R.id.product_image_post );
         trashButton = findViewById ( R.id.trash_btn_main );
 
-        fireBaseAuth = fireBaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-        categories = new ArrayList<>();
-        categories.add("Select a category");
-        displayCategorySpinner();
+        fireBaseAuth = fireBaseAuth.getInstance ();
+        db = FirebaseFirestore.getInstance ();
+        categories = new ArrayList<> ();
+        categories.add ( "Select a category" );
+        displayCategorySpinner ();
 
-        takePhotoButton.setOnClickListener(this);
-        uploadPhotoButton.setOnClickListener(this);
-        postProductButton.setOnClickListener(this);
-        productCategorySpinner.setOnItemSelectedListener(this);
+        takePhotoButton.setOnClickListener ( this );
+        uploadPhotoButton.setOnClickListener ( this );
+        postProductButton.setOnClickListener ( this );
+        productCategorySpinner.setOnItemSelectedListener ( this );
         trashButton.setOnClickListener ( this );
 
         // initialize the autocomplete
-        autocompleteSupportFragment.setPlaceFields (Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME));
+        autocompleteSupportFragment.setPlaceFields ( Arrays.asList ( Place.Field.LAT_LNG, Place.Field.NAME ) );
         autocompleteSupportFragment.setHint ( "Enter your item address" );
-        ((EditText)findViewById ( R.id.places_autocomplete_search_input )).setTextSize(15);
-        autocompleteSupportFragment.setOnPlaceSelectedListener(this);
+        ((EditText) findViewById ( R.id.places_autocomplete_search_input )).setTextSize ( 15 );
+        autocompleteSupportFragment.setOnPlaceSelectedListener ( this );
 
     }
 
     private void displayCategorySpinner() {
-        CollectionReference collectionReference = db.collection("categories");
-        collectionReference.get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        CollectionReference collectionReference = db.collection ( "categories" );
+        collectionReference.get ()
+                .addOnCompleteListener ( new OnCompleteListener<QuerySnapshot> () {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()) {
-                            for(QueryDocumentSnapshot document : task.getResult()) {
-                                categories.add(document.getId());
+                        if (task.isSuccessful ()) {
+                            for (QueryDocumentSnapshot document : task.getResult ()) {
+                                categories.add ( document.getId () );
                             }
-                            Log.d("What is in my list?", categories.toString());
+                            Log.d ( "What is in my list?", categories.toString () );
                         } else {
-                            Log.d("ERROR: ", "error while getting document: ", task.getException());
+                            Log.d ( "ERROR: ", "error while getting document: ", task.getException () );
                         }
                     }
-                });
+                } );
 
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, categories);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        productCategorySpinner.setAdapter(adapter);
+        ArrayAdapter adapter = new ArrayAdapter ( this, R.layout.support_simple_spinner_dropdown_item, categories );
+        adapter.setDropDownViewResource ( R.layout.support_simple_spinner_dropdown_item );
+        productCategorySpinner.setAdapter ( adapter );
     }
 
     private void postProduct() {
-        final String name = productNameText.getText().toString().trim();
-        final String description = productDescriptionText.getText().toString().trim();
+        final String name = productNameText.getText ().toString ().trim ();
+        final String description = productDescriptionText.getText ().toString ().trim ();
         final String category = chosenCategory;
 
-        final Double price = Double.parseDouble(productPriceText.getText().toString().trim());
-        final CollectionReference collectionReference = db.collection("products");
+        final Double price = Double.parseDouble ( productPriceText.getText ().toString ().trim () );
+        final CollectionReference collectionReference = db.collection ( "products" );
 
-        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(description)
-        && !TextUtils.isEmpty(category) && price != null && productImageUri != null) {
+        if (!TextUtils.isEmpty ( name ) && !TextUtils.isEmpty ( description )
+                && !TextUtils.isEmpty ( category ) && price != null && productImageUri != null) {
             final StorageReference filepath = storageReference
-                    .child("Test")
-                    .child("product_image" + Timestamp.now().getSeconds());
+                    .child ( "Test" )
+                    .child ( "product_image" + Timestamp.now ().getSeconds () );
 
-            filepath.putFile(productImageUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            filepath.putFile ( productImageUri )
+                    .addOnSuccessListener ( new OnSuccessListener<UploadTask.TaskSnapshot> () {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            filepath.getDownloadUrl ().addOnSuccessListener ( new OnSuccessListener<Uri> () {
                                 @Override
                                 public void onSuccess(Uri uri) {
-                                    Product product = new Product();
-                                    product.setName(name);
-                                    product.setDescription(description);
-                                    product.setCoordinates (geoPoint);
-                                    product.setPrice(price);
-                                    product.setPhotoURL(uri.toString());
-                                    product.setDateAdded(new Timestamp(new Date()));
-                                    product.setCategory(chosenCategory);
-                                    product.setUserId(currentUserId);
+                                    Product product = new Product ();
+                                    product.setName ( name );
+                                    product.setDescription ( description );
+                                    product.setCoordinates ( geoPoint );
+                                    product.setPrice ( price );
+                                    product.setPhotoURL ( uri.toString () );
+                                    product.setDateAdded ( new Timestamp ( new Date () ) );
+                                    product.setCategory ( chosenCategory );
+                                    product.setUserId ( currentUserId );
 
                                     ArrayList<String> pendingOffers = new ArrayList<> ();
                                     product.setPendingOffers ( pendingOffers );
-                                    collectionReference.add(product);
+                                    collectionReference.add ( product );
                                 }
-                            });
+                            } );
                         }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
+                    } )
+                    .addOnFailureListener ( new OnFailureListener () {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            Log.d("ERROR WHILE POSTING NEW PRODUCT: ", e.getMessage());
+                            Log.d ( "ERROR WHILE POSTING NEW PRODUCT: ", e.getMessage () );
                         }
-                    });
+                    } );
         }
     }
 
 
-
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
+        switch (view.getId ()) {
             case R.id.take_photo_button:
-                Intent takePictureIntent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                Intent takePictureIntent = new Intent ( MediaStore.ACTION_IMAGE_CAPTURE );
+                if (takePictureIntent.resolveActivity ( getPackageManager () ) != null) {
                     File photoFile = null;
                     try {
                         photoFile = createImageFile ();
 
                     } catch (IOException ex) {
-                        Log.e("ERROR: ", "Error occurred while creating the file");
+                        Log.e ( "ERROR: ", "Error occurred while creating the file" );
                     }
-                    if(photoFile != null) {
+                    if (photoFile != null) {
                         Uri photoURI = FileProvider.getUriForFile ( this, "com.example.blocal", photoFile );
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        takePictureIntent.putExtra ( MediaStore.EXTRA_OUTPUT, photoURI );
+                        startActivityForResult ( takePictureIntent, REQUEST_IMAGE_CAPTURE );
                     }
                 }
                 break;
             case R.id.upload_photo_button:
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, GALLERY_CODE);
+                Intent galleryIntent = new Intent ( Intent.ACTION_GET_CONTENT );
+                galleryIntent.setType ( "image/*" );
+                startActivityForResult ( galleryIntent, GALLERY_CODE );
                 break;
             case R.id.post_product_button:
-                postProduct();
-                startActivity(new Intent(PostProductActivity.this, MainActivity.class));
-                finish();
+                postProduct ();
+                startActivity ( new Intent ( PostProductActivity.this, MainActivity.class ) );
+                finish ();
                 break;
             case R.id.trash_btn_main:
                 productImageUri = null;
@@ -234,17 +233,17 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
     private File createImageFile() throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat ("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String timeStamp = new SimpleDateFormat ( "yyyyMMdd_HHmmss", Locale.US ).format ( new Date () );
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir( Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
+        File storageDir = getExternalFilesDir ( Environment.DIRECTORY_PICTURES );
+        File image = File.createTempFile (
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
                 storageDir      /* directory */
         );
 
         // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
+        currentPhotoPath = image.getAbsolutePath ();
 
         return image;
     }
@@ -252,32 +251,32 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
-            if(data != null) {
-                productImageUri = data.getData();
-                Picasso.get().load(productImageUri).fit().centerCrop().into(productImageView);
-                productImageView.setBackgroundColor(0x00000000);
-                takePhotoButton.setVisibility(View.INVISIBLE);
-                uploadPhotoButton.setVisibility(View.INVISIBLE);
+        super.onActivityResult ( requestCode, resultCode, data );
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                productImageUri = data.getData ();
+                Picasso.get ().load ( productImageUri ).fit ().centerCrop ().into ( productImageView );
+                productImageView.setBackgroundColor ( 0x00000000 );
+                takePhotoButton.setVisibility ( View.INVISIBLE );
+                uploadPhotoButton.setVisibility ( View.INVISIBLE );
             }
         }
 
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            if(data != null) {
-                productImageUri = Uri.fromFile(new File(currentPhotoPath));
-                Picasso.get().load(productImageUri).fit().centerCrop().into(productImageView);
-                productImageView.setBackgroundColor(0x00000000);
-                takePhotoButton.setVisibility(View.INVISIBLE);
-                uploadPhotoButton.setVisibility(View.INVISIBLE);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null) {
+                productImageUri = Uri.fromFile ( new File ( currentPhotoPath ) );
+                Picasso.get ().load ( productImageUri ).fit ().centerCrop ().into ( productImageView );
+                productImageView.setBackgroundColor ( 0x00000000 );
+                takePhotoButton.setVisibility ( View.INVISIBLE );
+                uploadPhotoButton.setVisibility ( View.INVISIBLE );
             }
         }
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        chosenCategory = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(this, "Selected: " + chosenCategory, Toast.LENGTH_SHORT).show();
+        chosenCategory = adapterView.getItemAtPosition ( i ).toString ();
+        Toast.makeText ( this, "Selected: " + chosenCategory, Toast.LENGTH_SHORT ).show ();
     }
 
     @Override
@@ -287,9 +286,9 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     protected void onStart() {
-        super.onStart();
-        user = fireBaseAuth.getCurrentUser();
-        currentUserId = user.getUid();
+        super.onStart ();
+        user = fireBaseAuth.getCurrentUser ();
+        currentUserId = user.getUid ();
     }
 
     @Override
@@ -302,6 +301,6 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onError(@NonNull Status status) {
-        Log.e("PostProductActivity", "An error occurred: " + status);
+        Log.e ( "PostProductActivity", "An error occurred: " + status );
     }
 }
