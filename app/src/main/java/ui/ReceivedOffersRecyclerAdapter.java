2bcc84f3c9812d64ea50c79486915075e0106cfe
Elise Pham <elise.pham88@gmail.com>
2019-07-27 19:15:40 -0700

    in progress of accept deny work flow

--- app/src/main/java/ui/ReceivedOffersRecyclerAdapter.java
+++ app/src/main/java/ui/ReceivedOffersRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,11 +13,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.blocal.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Date;
 import java.util.List;
+
+import at.markushi.ui.CircleButton;
 
 public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<ReceivedOffersRecyclerAdapter.ViewHolder> {
     private Context context;
@@ -41,7 +48,9 @@ public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<Received
 
         // query the database to find the matching price
         final FirebaseFirestore db = FirebaseFirestore.getInstance ();
-        DocumentReference df = db.collection ( "offers" ).document ( offerId );
+        final CollectionReference offers = db.collection ( "offers" );
+
+        final DocumentReference df = offers.document ( offerId );
         df.get ()
                 .addOnCompleteListener ( new OnCompleteListener<DocumentSnapshot> () {
                     @Override
@@ -50,6 +59,23 @@ public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<Received
                             DocumentSnapshot document = task.getResult ();
                             String buyerId = document.get ( "buyerId" ).toString ();
                             holder.offerAmount.setText ( "$" + document.get ( "price" ).toString () );
+
+                            // set visibility of accept and deny button
+                            String status = document.get ( "status" ).toString ();
+
+                            if (status.equals ( "pending" )) {
+                                holder.acceptButton.setVisibility ( View.VISIBLE );
+                                holder.rejectButton.setVisibility ( View.VISIBLE );
+                            }
+
+                            if (status.equals ( "accepted" )) {
+                                holder.rejectButton.setVisibility ( View.INVISIBLE );
+                            }
+
+                            if (status.equals ( "rejected" )) {
+                                holder.acceptButton.setVisibility ( View.INVISIBLE );
+                            }
+
                             // query the database to find the matching buyer name
                             DocumentReference buyer = db.collection ( "users" ).document ( buyerId );
                             buyer.get ()
@@ -67,16 +93,62 @@ public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<Received
                     }
                 } );
 
-//        holder.itemView.setOnClickListener ( new View.OnClickListener () {
-//            @Override
-//            public void onClick(View view) {
-//                Log.i ( "Product card: ", "is clicked" );
-//                Intent intent = new Intent ( context, ProductDetailActivity.class ).putExtra ( "product", product );
-//                intent.setExtrasClassLoader ( Offer.class.getClassLoader () );
-//                context.startActivity ( intent );
-//            }
-//        } );
+        handleAcceptOffer ( holder, df, offerId, offers );
 
+        handleRejectOffer ( holder, df, offerId, offers );
+
+    }
+
+    private void handleAcceptOffer(@NonNull final ViewHolder holder,
+                                   final DocumentReference df,
+                                   final String offerId,
+                                   final CollectionReference offers) {
+        holder.acceptButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                // update status of the existing offer
+                df.update ( "status", "accepted" );
+                df.update("dateUpdated", new Timestamp ( new Date () ) );
+                df.update("productState", true);
+
+                // query the offers collection
+                for(String id : receivedOffers) {
+                    if(offerId.equals(id)) {
+                        continue;
+                    }
+                    offers.document (id).update ( "status", "rejected" );
+                }
+                queryProduct ( df );
+            }
+        } );
+    }
+
+    private void queryProduct(final DocumentReference df) {
+        df.get()
+                .addOnCompleteListener ( new OnCompleteListener<DocumentSnapshot> () {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful ()) {
+                            DocumentSnapshot document = task.getResult ();
+                            String productId = document.get("productId").toString ();
+                            FirebaseFirestore db = FirebaseFirestore.getInstance ();
+                            DocumentReference soldProduct = db.collection ( "products" ).document (productId);
+                            soldProduct.update ( "sold", true );
+                        }
+                    }
+                } );
+    }
+
+    private void handleRejectOffer(@NonNull final ViewHolder holder,
+                                   final DocumentReference df,
+                                   final String offerId,
+                                   final CollectionReference offers) {
+        holder.rejectButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+
+            }
+        } );
     }
 
 
@@ -89,6 +161,8 @@ public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<Received
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView offerAmount;
         public TextView buyerName;
+        public CircleButton acceptButton;
+        public CircleButton rejectButton;
 
         public ViewHolder(@NonNull View itemView, Context ctx) {
             super ( itemView );
@@ -96,6 +170,8 @@ public class ReceivedOffersRecyclerAdapter extends RecyclerView.Adapter<Received
 
             offerAmount = itemView.findViewById ( R.id.offer_amount_list );
             buyerName = itemView.findViewById ( R.id.offer_buyer_name );
+            acceptButton = itemView.findViewById ( R.id.offer_accept_btn );
+            rejectButton = itemView.findViewById ( R.id.offer_deny_btn );
         }
     }
 
